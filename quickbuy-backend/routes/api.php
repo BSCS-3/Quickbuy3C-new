89f84b4d2636e89c087e23b7c232cf262da67d01
Markdiocre <mark.manuel.business@gmail.com>
2025-03-14 22:51:24 +0800

    Working role middlewares

--- quickbuy-backend/routes/api.php
+++ quickbuy-backend/routes/api.php
@@ -2,19 +2,48 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\AuthController;
+
+use App\Http\Middleware\RoleMiddleware;
+
 use App\Http\Controllers\ItemController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Admin Routes -> http://127.0.0.1:8000/api/admin
+Route::prefix('admin')
+    ->middleware(['auth:sanctum', RoleMiddleware::class.':admin'])
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/', 'hello');
+    });
 
-#http://127.0.0.1:8000/api/users/get_single/:id
-Route::controller(UserController::class)->prefix("users")->group(function(){
-    Route::get('/hi', "say_hello");
-});
 
-Route::controller(ItemController::class)->prefix("items")->group(function(){
-    Route::get('/', "find_all");
-    Route::post('/',"create");
-});
+// Seller Routes
+Route::prefix('seller')
+    ->middleware(['auth:sanctum', RoleMiddleware::class.':admin'])
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/', 'hello');
+    });
+
+// Customer Routes
+Route::prefix('customer')
+    ->middleware(['auth:sanctum', RoleMiddleware::class.':admin'])
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/', 'hello');
+    });
+
+
+//Auth Routes 
+/*
+POST /api/login/admin → For Admin Login
+POST /api/login/seller → For Seller Login
+POST /api/login/customer → For Customer Login
+*/ 
+Route::post('/login/{role}', [AuthController::class, 'login'])->where('role', 'admin|seller|customer');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
